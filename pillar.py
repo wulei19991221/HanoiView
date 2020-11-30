e2029483611a25a02b99c3c060d78b8f68732ebf
wulei19991221 <wulei7217@gmail.com>
2020-11-30 22:13:48 +0800

    新增注释

--- pillar.py
+++ pillar.py
@@ -8,15 +8,21 @@ from random import choice
 
 
 class Pillar:
+    # 柱子颜色
     colors = ('#1abc9c', '#2ecc71', '#3498db', '#9b59b6', '#e67e22', '#2c3e50')
+    # 柱子宽度和高度
     __height = 400
     __width = 150
+    center_x = 0
+    center_y = __height
 
     def __init__(self, pen: t.Pen, x: int, y: int, width: int, name: str):
         self.__pen = pen
         self.x = x
         self.y = y
         self.__width = width
+        self.center_x = self.x + self.__width // 2
+        self.center_y = self.y
         self.name = name
         self.initPen()
 
@@ -29,22 +35,29 @@ class Pillar:
         self.__pen.goto(self.x, self.y)
         self.__pen.down()
 
-    def home(self):
-        self.__pen.goto(self.x, self.y)
+    def toCenter(self):
+        self.__pen.up()
+        self.__pen.goto(self.center_x, self.center_y)
+        self.__pen.down()
 
+    # 返回随机颜色代码
     def getColor(self):
         return choice(self.colors)
 
+    # 画柱子
     def draw(self):
+        # 画底盘
         self.__pen.forward(self.__width)
-        self.__pen.backward(self.__width // 2)
+        self.toCenter()
+        # 画支柱
         self.__pen.right(90)
         self.__pen.forward(self.__height)
-        self.__pen.up()
-        self.__pen.goto(self.x + self.__width // 2, self.y + 50)
-        self.__pen.down()
+        self.toCenter()
+        # 画字母
+        self.__pen.forward(-50)
         self.__pen.color(self.getColor())
         self.__pen.write(self.name, align='center', font=('Arial', 18, 'normal'))
 
+    # 返回柱子底部中心坐标
     def getCenterPosition(self):
-        return self.x + self.__width // 2, self.y
+        return self.center_x, self.center_y
